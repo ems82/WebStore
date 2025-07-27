8f29769ca3f301c5ca935325c890562e1e4a8a25
ems82 <ems82@ya.ru>
2025-07-27 10:40:13 +0300

    "Пртвет мир " по RUS

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,7 +1,15 @@
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+app.MapGet("/", static async context =>
+{
+    context.Response.ContentType = "text/plain;charset=utf-8";
+    await context.Response.WriteAsync("Привет Мир!!!!!", Encoding.UTF8);
+});
+  
 
 app.Run();
