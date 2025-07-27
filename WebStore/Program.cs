da28ebac1faf6a3b58831b0fa438fff87713a54f
ems82 <ems82@ya.ru>
2025-07-27 11:18:53 +0300

    Запрос конфигурации из appsetting.json

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,14 +1,16 @@
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var logLevel = builder.Configuration["CustomGreetings"];
+
 builder.Services.AddRazorPages();
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
-app.MapGet("/", static async context =>
+app.MapGet("/", async context =>
 {
     context.Response.ContentType = "text/plain;charset=utf-8";
-    await context.Response.WriteAsync("Привет Мир!!!!!", Encoding.UTF8);
+    await context.Response.WriteAsync("Привет Мир!!!!!" + ":\n" + logLevel, Encoding.UTF8);
 });
   
 
