01608aa41405807b204b497105575ba2c90ddb41
ems82 <ems82@ya.ru>
2025-08-01 06:26:02 +0300

    Создание HomeController

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Conflict("Home");
+        }
+        public IActionResult SomeAction()
+        {
+            return Conflict("SHome");
+        }
+
+    }
+}
--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -2,16 +2,25 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 //var greetings = builder.Configuration["CustomGreetings"];
+builder.Services.AddMvc();
 
 builder.Services.AddRazorPages();
 var app = builder.Build();
 
+
 //app.MapGet("/", () => "Hello World!");
-app.MapGet("/", async context =>
+app.MapGet("/greeting", async context =>
 {
     context.Response.ContentType = "text/plain;charset=utf-8";
     await context.Response.WriteAsync("Привет Мир!!!!!" + ":\n" + builder.Configuration["CustomGreetings"], Encoding.UTF8);
 });
-  
+app.MapControllerRoute(
+    "default",
+    "{controller=Home}/{action=Index}/{id?}");  // http://localhost:5000/Home/Index/id
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+//  app.UseMvc();
 
 app.Run();
